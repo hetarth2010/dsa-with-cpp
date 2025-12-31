7866b7c0bafc73e6857819843b2ee22f4c03d484
hetarth2010 <bhavsarhetarth240@gmail.com>
2025-12-31 16:09:06 +0530

    Add files via upload

---
+++ grade-system.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks;
+
+    cout << "Enter your marks (0-100): ";
+    cin >> marks;
+
+    if (marks < 0 || marks > 100) {
+        cout << "Invalid Marks!";
+    }
+    else if (marks >= 90) {
+        cout << "Grade: A+";
+    }
+    else if (marks >= 80) {
+        cout << "Grade: A";
+    }
+    else if (marks >= 70) {
+        cout << "Grade: B";
+    }
+    else if (marks >= 60) {
+        cout << "Grade: C";
+    }
+    else if (marks >= 50) {
+        cout << "Grade: D";
+    }
+    else {
+        cout << "Grade: F (Fail)";
+    }
+
+    return 0;
+}
