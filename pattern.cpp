9ff8832b461c1f5fab60a38313ef3421ee879054
hetarth2010 <bhavsarhetarth240@gmail.com>
2026-04-10 11:56:25 +0530

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -7,8 +7,8 @@ int main() {
     cout << "Enter number of rows: ";
     cin >> n;
 
-    for (int i = 1; i <= n; i++) {          // Outer loop for rows
-        for (int j = 1; j <= i; j++) {      // Inner loop for columns
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {     
             cout << j;
         }
         cout << endl;
@@ -46,15 +46,63 @@ int main() {
     cout << "Enter number of rows: ";
     cin >> n;
 
-    for (int i = 1; i <= n; i++) {              // Outer loop for rows
-        
-        // Print spaces
+    for (int i = 1; i <= n; i++) {          
         for (int j = 1; j <= n - i; j++) {
             cout << " ";
         }
 
-        // Print stars
+    
         for (int k = 1; k <= i; k++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
+
+//task 4
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (int i = n; i >= 1; i--) {     
+        for (int j = 0; j < n - i; j++) {
+            cout << " ";
+        }
+
+        for (int k = 1; k <= i; k++) {
+            cout << "0""1" ;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
+//task 5
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {         
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        
+        for (int k = 1; k <= (2 * i - 1); k++) {
             cout << "*";
         }
 
