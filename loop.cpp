d5b20b5308e8163fb195117fc340b03b18c4bf13
hetarth2010 <bhavsarhetarth240@gmail.com>
2026-04-10 11:29:02 +0530

    Refactor loop.cpp to include multiple main functions

--- loop.cpp
+++ loop.cpp
@@ -17,3 +17,61 @@ int main() {
 
     return 0;
 }
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, count = 0;
+
+    cout << "Enter any number: ";
+    cin >> num;
+
+    // Handle negative numbers
+    if (num < 0) {
+        num = -num;
+    }
+
+    // Special case for 0
+    if (num == 0) {
+        count = 1;
+    } else {
+        while (num != 0) {
+            num = num / 10; // remove last digit
+            count++;
+        }
+    }
+
+    cout << "Total number of digits: " << count;
+
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, first, last, sum;
+
+    cout << "Enter any number: ";
+    cin >> num;
+
+    // Handle negative numbers
+    if (num < 0) {
+        num = -num;
+    }
+
+    // Get last digit
+    last = num % 10;
+
+    // Find first digit
+    while (num >= 10) {
+        num = num / 10;
+    }
+    first = num;
+
+    // Calculate sum
+    sum = first + last;
+
+    cout << "The sum of the first and the last digit: " << sum;
+
+    return 0;
+}
